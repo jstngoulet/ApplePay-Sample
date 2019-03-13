651e61de7ef0df027cd916f9c712ffd1e22ae2a2
Tizzle Goulet <tizzle@hyrecar.com>
2019-03-12 17:00:48 -0700

    Created project

---
+++ Apple Pay Sample/Apple Pay Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Apple Pay Sample
+//
+//  Created by Tizzle Goulet on 3/12/19.
+//  Copyright © 2019 HyreCar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
